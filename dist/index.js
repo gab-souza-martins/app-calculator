bdd9b635906607237880bd4536f5a8b9c0364149
Grey-B <gabriellemener@gmail.com>
2025-07-09 10:54:13 -0300

    Adicionada animação para o toggle

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,19 @@
 "use strict";
 let theme = localStorage.getItem("theme");
 const themeToggleBox = document.getElementById("theme-toggle-box");
+const themeToggleBtn = document.getElementById("theme-toggle-btn");
 const enableThemeOne = () => {
     document.body.classList.remove("theme3");
-    if (themeToggleBox) {
-        themeToggleBox.style.justifyContent = "left";
+    if (themeToggleBtn) {
+        themeToggleBtn.style.transform = "translateX(0%)";
     }
     localStorage.setItem("theme", "themeOne");
     theme = "themeOne";
 };
 const enableThemeTwo = () => {
     document.body.classList.add("theme2");
-    if (themeToggleBox) {
-        themeToggleBox.style.justifyContent = "center";
+    if (themeToggleBtn) {
+        themeToggleBtn.style.transform = "translateX(120%)";
     }
     localStorage.setItem("theme", "themeTwo");
     theme = "themeTwo";
@@ -20,8 +21,8 @@ const enableThemeTwo = () => {
 const enableThemeThree = () => {
     document.body.classList.remove("theme2");
     document.body.classList.add("theme3");
-    if (themeToggleBox) {
-        themeToggleBox.style.justifyContent = "right";
+    if (themeToggleBtn) {
+        themeToggleBtn.style.transform = "translateX(240%)";
     }
     localStorage.setItem("theme", "themeThree");
     theme = "themeThree";
--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,21 @@ let theme = localStorage.getItem("theme");
 
 const themeToggleBox: HTMLElement | null =
    document.getElementById("theme-toggle-box");
+const themeToggleBtn: HTMLElement | null =
+   document.getElementById("theme-toggle-btn");
 
 const enableThemeOne = (): void => {
    document.body.classList.remove("theme3");
-   if (themeToggleBox) {
-      themeToggleBox.style.justifyContent = "left";
+   if (themeToggleBtn) {
+      themeToggleBtn.style.transform = "translateX(0%)";
    }
    localStorage.setItem("theme", "themeOne");
    theme = "themeOne";
 };
 const enableThemeTwo = (): void => {
    document.body.classList.add("theme2");
-   if (themeToggleBox) {
-      themeToggleBox.style.justifyContent = "center";
+   if (themeToggleBtn) {
+      themeToggleBtn.style.transform = "translateX(120%)";
    }
    localStorage.setItem("theme", "themeTwo");
    theme = "themeTwo";
@@ -22,8 +24,8 @@ const enableThemeTwo = (): void => {
 const enableThemeThree = (): void => {
    document.body.classList.remove("theme2");
    document.body.classList.add("theme3");
-   if (themeToggleBox) {
-      themeToggleBox.style.justifyContent = "right";
+   if (themeToggleBtn) {
+      themeToggleBtn.style.transform = "translateX(240%)";
    }
    localStorage.setItem("theme", "themeThree");
    theme = "themeThree";
